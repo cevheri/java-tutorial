50cd28ec8ccede7e163ac144c17a9c322e590a13
Cevheri <cevheribozoglan@gmail.com>
2021-06-10 22:07:49 +0300

    Added Java Polymorphism example

--- src/main/java/com/cevher/java/polymorphism/JavaPolymorphism.java
+++ src/main/java/com/cevher/java/polymorphism/JavaPolymorphism.java
@@ -1,30 +1,55 @@
 package com.cevher.java.polymorphism;
 
+// ref: https://www3.ntu.edu.sg/home/ehchua/programming/java/JavaGeneric.html
+
+import java.util.ArrayList;
+
 public class JavaPolymorphism {
     public static void main(String[] args) {
         // Substitution: Reference to SuperType can accept instance of SuperType and its subclasses
-        SuperType c1Ref = new SubType();  // substituted with SuperType subclass' instance
-        c1Ref.methodSuperType();     //SuperType runs methodSuperType()
-        //c1Ref.methodSubType();   // CANNOT reference subclass method
+        SuperType masterReference = new SubType();  // substituted with SuperType subclass' instance
+        masterReference.methodSuperType();     //SuperType runs methodSuperType()
+        //masterReference.methodSubType();   // CANNOT reference subclass method
         //error: cannot find symbol
 
         // Polymorphism: run the overridden version
-        c1Ref.sayHello();    //SubType runs overridden sayHello()
+        masterReference.sayHello();    //SubType runs overridden sayHello()
 
         // Up-casting is type-safe, does not require explicit type cast operator
-        SuperType c1Ref2 = new SubType();
+        SuperType masterReference2 = new SubType();
         // Down-casting is NOT type-safe, require explicit type cast operator
-        SubType c2Ref = (SubType)c1Ref2;
-        c2Ref.methodSubType();
-        //SubType c2Ref = c1Ref2;
+        SubType subClassReference = (SubType) masterReference2;
+        subClassReference.methodSubType();
+        //SubType subClassReference = masterReference2;
         //error: incompatible types: SuperType cannot be converted to SubType
+
+        ISuperType iSuperType = new SubType();
+        // cannot access: iSuperType.methodSubType();
+
     }
 }
-class SuperType {
-    public void sayHello() { System.out.println("SuperType runs sayHello()"); }
-    public void methodSuperType() { System.out.println("SuperType runs methodSuperType()"); }
+
+interface ISuperType{
+
 }
-class SubType extends SuperType {  // SubType is a subclass of SuperType
-    @Override public void sayHello() { System.out.println("SubType runs overridden sayHello()"); }
-    public void methodSubType() { System.out.println("SubType runs methodSubType()"); }
+
+class SuperType {
+    public void sayHello() {
+        System.out.println("SuperType runs sayHello()");
+    }
+
+    public void methodSuperType() {
+        System.out.println("SuperType runs methodSuperType()");
+    }
+}
+
+class SubType extends SuperType implements ISuperType{  // SubType is a subclass of SuperType
+    @Override
+    public void sayHello() {
+        System.out.println("SubType runs overridden sayHello()");
+    }
+
+    public void methodSubType() {
+        System.out.println("SubType runs methodSubType()");
+    }
 }
