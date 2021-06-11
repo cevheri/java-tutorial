07491237a6030798288df8ff2d7d716d80e6b4c8
Cevheri <cevheribozoglan@gmail.com>
2021-06-11 21:03:54 +0300

    added custom Runnable example

---
+++ src/main/java/com/cevher/java/multithread/CustomRunnable.java
@@ -0,0 +1,17 @@
+package com.cevher.java.multithread;
+
+class MyRunnable extends MyThread implements Runnable {
+    public MyRunnable(String name) {
+        super(name);
+    }
+
+    // provide implementation to abstract method run()
+    @Override
+    public void run() {
+        // Thread's running behavior
+    }
+
+    // constructors, other variables and methods
+}
+public class CustomRunnable {
+}
