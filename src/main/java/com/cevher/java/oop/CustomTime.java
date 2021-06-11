151749dbb91c966970dd70e521956337d8039c39
Cevheri <cevheribozoglan@gmail.com>
2021-06-11 15:09:54 +0300

    Case 1: valid input: int hour = 23, minute = 58, second = 58; Case 2: invalid input: int hour = 24, minute = 58, second = 58;

--- src/main/java/com/cevher/java/oop/CustomTime.java
+++ src/main/java/com/cevher/java/oop/CustomTime.java
@@ -33,6 +33,44 @@ public class CustomTime {
         //23:59:59
         System.out.println(t1.nextSecond().nextSecond().nextSecond());  // chaining
         //00:00:02
+
+        ///////////////////////////////////////////////////////////////////////////////
+
+        // Case 1: valid input
+        //int hour = 23, minute = 58, second = 58;
+        // Case 2: invalid input
+        int hour = 24, minute = 58, second = 58;
+        Time t12;
+
+        //t12 = new Time(second, minute, hour);
+        // Without try-catch, the program will terminate abruptly if exception thrown
+
+        try {
+            t12 = new Time(second, minute, hour);
+            // If input is invalid, throw exception. Skip the rest, goto "catch".
+            // Else complete "try", skip "catch"
+            System.out.println("valid input");
+        } catch (IllegalArgumentException ex) {
+            // You have the opportunity to do something to recover from the error.
+            ex.printStackTrace();  // print error messages
+            System.out.println("Error in input. Set to default value");
+            // You should ask the user to provide the valid input instead!
+            t12 = new Time();
+        }
+
+        System.out.println("Time is " + t12);
+        System.out.println("Life goes on...");
+
+        // Case 1 output
+        //valid input
+        //Time is 23:58:58
+        //Life goes on...
+
+        // Case 2 output
+        //java.lang.IllegalArgumentException: invalid hour
+        //Error in input. Set to default value
+        //Time is 00:00:00
+        //Life goes on...
     }
 }
 /**
